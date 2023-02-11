445be0c27cd0107758b6993a77640967849e88ed
creesler <60352120+creesler@users.noreply.github.com>
2023-02-11 22:40:03 +0800

    Update index.js
    removed slash from the CORRS

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const app = express()
 
 app.use(
     cors({
-        origin: "https://crmv2.netlify.app/",
+        origin: "https://crmv2.netlify.app",
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
         credentials: true
 
